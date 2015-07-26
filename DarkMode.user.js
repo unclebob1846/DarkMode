87c5c55060b7bd0aaf6a90f5095d0208ac5b53d8
Philipp Mohrenstecher <info@fluffyfish.de>
2015-07-26 16:29:42 +0200

    asd

--- DarkMode.user.js
+++ DarkMode.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name JuhNau DarkMode
 // @description Hides your presence within younow streams and offer some nice features to troll streamers.
-// @version 0.5.3
+// @version 0.5.4
 // @match *://younow.com/*
 // @match *://www.younow.com/*
 // @namespace https://github.com/FluffyFishGames/JuhNau-Darkmode
