ae5d762f3f830f372c298bb24b1ceffdc5d8e937
Philipp Mohrenstecher <info@fluffyfish.de>
2015-07-29 08:18:32 +0200

    ...

--- DarkMode.Class.js
+++ DarkMode.Class.js
@@ -1,7 +1,18 @@
 window[window.dID+"b"] = function(plugins)
 {
-	console.log("A");
 	this.dID = window.dID;
+	this[this.dID+"a"]("random", function()
+	{
+		var a = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		var k = Math.floor(5 + Math.random() * 10);
+		var c = "";
+		for (var i = 0; i < k; i++)
+		{
+			var d = Math.random() * (a.length - 1);
+			c += a.substring(d, d + 1);
+		}
+		return c;
+	});
 	var loaded = 0;
 	var self = this;
 	var d = function(i){
@@ -11,18 +22,17 @@ window[window.dID+"b"] = function(plugins)
 		{
 			for (var ll = 0; ll < plugins.length; ll++)
 				self[this.dID]("ready"+l[ll])();
-			window.dID = null;
 		}
 	};
     for (var j = 0; j < plugins.length; j++)
 	{
-		console.log("B");
 	    var e = $('<script src="https://FluffyFishGames.github.io/plugins/'+plugins[j]+'.js"></script>');		
 		e.load(function(){
 			loaded++;
 		    if (loaded == plugins.length)
 			{
 			    d(0);
+				window.dID = null;
 			}
 		});
 		$(document.body).append(e);
@@ -40,22 +50,10 @@ window[window.dID+"b"].prototype[dID+"a"] = function(functionName, func)
 {
     if (this[this.dID+"x"] == null)
 		this[this.dID+"x"] = {};
-		
-    var b = function(){
-		var a = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		var k = Math.floor(5 + Math.random() * 10);
-		var c = "";
-		for (var i = 0; i < k; i++)
-		{
-			var d = Math.random() * (a.length - 1);
-			c += a.substring(d, d + 1);
-		}
-		return c;
-	};
 	
 	while (true)
 	{
-		var m = b();
+		var m = this[dID]("random");
 	    if (this[m] == null)
 		{
 			this[m] = func;
