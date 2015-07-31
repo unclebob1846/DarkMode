48ec0a3f94c494ffc201d3766a92055f7daa8dec
Philipp Mohrenstecher <info@fluffyfish.de>
2015-07-31 10:02:47 +0200

    ...

--- plugins/Config.js
+++ plugins/Config.js
@@ -4,15 +4,15 @@ window[window.dID][window.dID+"a"]("bootConfig",
 		var load = {};
 		for (var i = 0; i < this.plugins.length; i++)
 		{
-			load[this.plugins[key]] = "https://fluffyfishgames.github.io/data/"+this.plugins[key]+".json";
+			load[this.plugins[key]] = "https://fluffyfishgames.github.io/data/"+this.plugins[i]+".json";
 		}
 		this.config = {};
 		var loaded = 0;
 		var loading = 0;
 		var self = this;
-	    var d = function(key)
+	    var d = function(key, url)
 		{
-			$.ajax(load[key], {
+			$.ajax(url, {
 				dataType: "json",
 				success: function(json, b, c)
 				{
@@ -33,7 +33,7 @@ window[window.dID][window.dID+"a"]("bootConfig",
 		var loading = 0;
 	    for (var key in load)
 		{
-			d(key);
+			d(key, load[key]);
 			loading++;
 	    }
 	}
