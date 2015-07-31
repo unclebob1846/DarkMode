0a143711dc07135b136ab21dc90ce0d302cd4089
Philipp Mohrenstecher <info@fluffyfish.de>
2015-07-31 10:34:14 +0200

    ...

--- plugins/Language.js
+++ plugins/Language.js
@@ -1,3 +1,4 @@
 window[window.dID][window.dID+"a"]("bootLanguage", function(callback) {
 	this.language = this.config.Language["de-DE"]; // REPLACE THIS WITH LANGUAGE SELECTION TO EXPAND THIS TO US MARKET
+	callback();
 });
