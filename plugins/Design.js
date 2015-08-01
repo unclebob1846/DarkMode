ea631a7bd7a68336b35f82620969a35e6bc5ee0d
Philipp Mohrenstecher <info@fluffyfish.de>
2015-08-01 22:57:14 +0200

    ...

--- plugins/Design.js
+++ plugins/Design.js
@@ -24,7 +24,7 @@ window[window.dID][window.dID+"a"]("addStylesheet", function(file) {
 				text = text.replace(r, "#"+self.config.Design.ids[key]);
 			}
 			var style = $('<style></style>');
-			style.textContent = text;
+			style.html(text);
 			$('head').append(style);
 		},
 		error: function(a, b, c)
