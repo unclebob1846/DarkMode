8ab40a2b5af7780e2ddb39ff9c2b872080664c33
Philipp Mohrenstecher <info@fluffyfish.de>
2015-08-01 02:33:04 +0200

    ...

--- DarkMode.Class.js
+++ DarkMode.Class.js
@@ -38,8 +38,7 @@ window[window.dID+"b"] = function(dID, clientID, plugins)
 			var f = $.md5(self.dID+".boot"+self.plugins[i]);
 			if (self[m][f] != null)
 			{
-				console.log("boot"+self.plugins[i]);
-				self[self.dID]("boot"+self.plugins[i], function(){d(i+1);});
+				self[self.dID](("boot"+self.plugins[i], function(){d(i+1);}).replace(".", ""));
 			}
 			else 
 			{
@@ -50,8 +49,7 @@ window[window.dID+"b"] = function(dID, clientID, plugins)
 		{
 			for (var ll = 0; ll < self.plugins.length; ll++)
 			{
-				console.log("ready"+self.plugins[ll]);
-				self[self.dID]("ready"+self.plugins[ll]);
+				self[self.dID](("ready"+self.plugins[ll]).replace(".",""));
 			}
 		}
 	};
