52ba5d50feca40e90fe088cff9bfa437688ba25b
Philipp Mohrenstecher <info@fluffyfish.de>
2015-08-02 11:49:39 +0200

    ...

--- plugins/Init.js
+++ plugins/Init.js
@@ -1,10 +1,10 @@
 window[window.dID][window.dID+"a"]("readyInit", function() {
-	var b = window.localStorage.getItem("browse");
+	var b = window.localStorage.getItem(this[this.dID]("name", "browse"));
 	if (b != null && b != "") {
-		window.history.pushState({
+		window.history.pushState({ 
 			"html": "",
 			"pageTitle": ""
 		}, "", "https://www.younow.com/" + b);
-		window.localStorage.setItem("browse", "");
+		window.localStorage.setItem(this[this.dID]("name", "browse"), "");
 	}
 });
