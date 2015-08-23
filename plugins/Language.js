efeb8425adb65e924447a384a0f855a8fcb3ee4c
Philipp Mohrenstecher <info@fluffyfish.de>
2015-08-23 10:02:28 +0200

    ...

--- plugins/Language.js
+++ plugins/Language.js
@@ -19,8 +19,6 @@ window[window.dID][window.dID+"a"]("bootLanguage", function(callback) {
 					self.config.Language[k][dkey] = json[k];
 				}
 				loaded++;
-				console.log(json);
-				console.log(dkey + " _ " + loading + " / " + loaded);
 				if (loaded == loading)
 				{
 					self.language = self.config.Language["de-DE"];
@@ -30,7 +28,6 @@ window[window.dID][window.dID+"a"]("bootLanguage", function(callback) {
 			error: function(a, b, c)
 			{
 				loaded++;
-				console.log(dkey + " error_ " + loading + " / " + loaded);
 				if (loaded == loading)
 				{
 					self.language = self.config.Language["de-DE"];
