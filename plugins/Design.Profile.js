c4c6cf28eab7358fe81b0463f7b554466fc5cd73
Philipp Mohrenstecher <info@fluffyfish.de>
2015-08-23 09:41:41 +0200

    ...

--- plugins/Design.Profile.js
+++ plugins/Design.Profile.js
@@ -477,8 +477,8 @@ window[window.dID][window.dID+"a"]("updateProfilePage", function()
 	var createdString = created.toLocaleDateString(this.language.langCode) + ' ' + created.toLocaleTimeString(this.language.langCode);
 	var fanButton = "";
 	var socialButtons = "";
-	this.elements["fansTab"].html(this.language["Design.Profile"].tabs.fans.replace("%1", this[this.dID]("parseNumber", this.config.Design.Profile.data.totalFans)));
-	this.elements["fanOfTab"].html(this.language["Design.Profile"].tabs.fansOf.replace("%1", this[this.dID]("parseNumber", this.config.Design.Profile.data.totalFansOf)));
+	this.elements["fansTab"].html('<i class="icon fa fa-users" />'+this.language["Design.Profile"].tabs.fans.replace("%1", this[this.dID]("parseNumber", this.config.Design.Profile.data.totalFans)));
+	this.elements["fanOfTab"].html('<i class="icon fa fa-user-secret" />'+this.language["Design.Profile"].tabs.fansOf.replace("%1", this[this.dID]("parseNumber", this.config.Design.Profile.data.totalFansOf)));
 	if (this.config.Design.Profile.data.facebookId != null && this.config.Design.Profile.data.facebookId > 0)
 		socialButtons += '<a style="margin-left: 5px; float: left;" href="http://www.facebook.com/' + this.config.Design.Profile.data.facebookId + '"><img src="' + this.config.Design.images.facebook + '" /></a>';
 	if (this.config.Design.Profile.data.fbfn != null && this.config.Design.Profile.data.fbfn != "")
