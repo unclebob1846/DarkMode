2db71f2ba30720edaf8064c85ae852dbd41dd456
Philipp Mohrenstecher <info@fluffyfish.de>
2015-08-23 08:21:34 +0200

    ...

--- plugins/Design.UserMenu.js
+++ plugins/Design.UserMenu.js
@@ -41,7 +41,7 @@ window[window.dID][window.dID+"a"]("addUserMenuItem", function(name, icon, callb
 window[window.dID][window.dID+"a"]("addUserMenuItemElement", function(name) {
 	if (this.config.loggedIn)
 	{
-		var liElement = $('<li><a><i class="fa fa-'+this.config.Design.UserMenu.buttons[name].icon+'"></i>'+name+'</a></li>');
+		var liElement = $('<li><a><i class="fa fa-'+this.config.Design.UserMenu.buttons[name].icon+' fa-2x" href></i><span>'+name+'</span></a></li>');
 		liElement.click(this.config.Design.UserMenu.buttons[name].callback);
 		this.elements["userMenuList"].append(liElement);
 	}
